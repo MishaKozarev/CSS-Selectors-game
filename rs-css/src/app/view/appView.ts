f0165a8c3b60878d1b084e23a02e1b2ad903bc53
MishaKozarev <mishaaaaa1986@mail.ru>
2023-06-23 17:17:11 +0300

    feat: add html markup

--- rs-css/src/app/view/appView.ts
+++ rs-css/src/app/view/appView.ts
@@ -1,19 +1,10 @@
-import LeftColumn from './leftColumn/leftColumn';
-import RightColumn from './rightColumn/rightColumn';
-
 class AppView {
     private body: HTMLElement;
-    private LeftColumn: LeftColumn;
-    private RightColumn: RightColumn;
     constructor() {
         this.body = document.body;
-        this.LeftColumn = new LeftColumn();
-        this.RightColumn = new RightColumn();
     }
     run() {
-        const LeftColumnHtml = this.LeftColumn.render();
-        const RightColumnHtml = this.RightColumn.render();
-        this.body.append(LeftColumnHtml, RightColumnHtml);
+        console.log(this.body);
     }
 }
 export default AppView;
